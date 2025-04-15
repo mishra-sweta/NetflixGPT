fe700f867a44612f68b9c13688e23360ace959d3
mishra-sweta <swetamishra.liluah@gmail.com>
2025-04-15 15:30:59 +0530

    validation of input fields in Login Page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,48 +1,81 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef("");
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleButtonClick = () => {
+    const message = checkValidData(
+      email.current.value,
+      password.current.value,
+      name.current.value
+    );
+    setErrorMessage(message);
+  };
+
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
   return (
-    <div>
-      <Header />
-      <div className="absolute">
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/fa4630b1-ca1e-4788-94a9-eccef9f7af86/web_tall_panel/IN-en-20250407-TRIFECTA-perspective_8be2cd93-f2e6-4e34-acba-05b716385704_large.jpg" />
+    <div className="relative">
+      <div className="absolute z-0">
+        <img
+          className="w-full object-cover"
+          alt="background"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/fa4630b1-ca1e-4788-94a9-eccef9f7af86/web_tall_panel/IN-en-20250407-TRIFECTA-perspective_8be2cd93-f2e6-4e34-acba-05b716385704_large.jpg"
+        />
       </div>
-
-      <form className="w-3/12 text-white absolute bg-black/80 my-36 mx-auto right-0 left-0 p-12 rounded-lg">
-        <h1 className="text-3xl font-bold my-1">
-          {isSignInForm ? "Sign In" : "Sign Up"}
-        </h1>
-        {!isSignInForm && (
+      <div className="relative z-10">
+        <Header />
+        <form
+          className="w-3/12 text-white absolute bg-black/80 my-36 mx-auto right-0 left-0 p-12 rounded-lg"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <h1 className="text-3xl font-bold my-1">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </h1>
+          {!isSignInForm && (
+            <input
+              ref={name}
+              className="p-2 my-3 w-full bg-gray-700 rounded"
+              type="text"
+              placeholder="Full Name"
+            />
+          )}
           <input
+            ref={email}
             className="p-2 my-3 w-full bg-gray-700 rounded"
-            type="text"
-            placeholder="Full Name"
+            type="email"
+            placeholder="Email Address"
           />
-        )}
-        <input
-          className="p-2 my-3 w-full bg-gray-700 rounded"
-          type="email"
-          placeholder="Email Address"
-        />
-        <input
-          className="p-2 my-3 w-full  bg-gray-700 rounded"
-          type="password"
-          placeholder="Password"
-        />
-        <button className="p-3 my-3 w-full bg-red-800 rounded">
-          {isSignInForm ? "Sign In" : "Sign Up"}
-        </button>
-        <p className="cursor-pointer text-sm" onClick={toggleSignInForm}>
-          {isSignInForm
-            ? "New to Netflix? Sign up now"
-            : "Already registered? Sign in now"}
-        </p>
-      </form>
+          <input
+            ref={password}
+            className="p-2 my-3 w-full  bg-gray-700 rounded"
+            type="password"
+            placeholder="Password"
+          />
+          <p className="text-red-800 text-sm">{errorMessage}</p>
+          <button
+            className="p-3 my-3 w-full bg-red-800 rounded"
+            onClick={handleButtonClick}
+          >
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </button>
+          <p className="cursor-pointer text-sm" onClick={toggleSignInForm}>
+            {isSignInForm
+              ? "New to Netflix? Sign up now"
+              : "Already registered? Sign in now"}
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
---
+++ src/utils/validate.js
@@ -0,0 +1,21 @@
+export const checkValidData = (email, password, name) => {
+  const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+    email
+  );
+
+  const isPasswordValid =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(
+      password
+    );
+
+  const isNameValid =
+    /(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})/.test(
+      name
+    );
+
+  if (!isEmailValid) return "Email is not valid";
+  if (!isPasswordValid) return "Password is not valid";
+  if (!isNameValid) return "Name is not valid";
+
+  return null;
+};
