6ad362935d7761dfe9810ce4ae16156e20b9285d
mishra-sweta <swetamishra.liluah@gmail.com>
2025-04-16 14:03:11 +0530

    made a customized hook for fetching movies and storing it

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -3,22 +3,10 @@ import Header from "./Header";
 import { API_OPTIONS } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  const dispatch = useDispatch();
-  const nowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json.results);
-    dispatch(addNowPlayingMovies(json.results));
-  };
-
-  useEffect(() => {
-    nowPlayingMovies();
-  }, []);
+  useNowPlayingMovies();
 
   return (
     <div>
