bb2df73e0d6774fefd77107e1f4e15e41d816a63
mishra-sweta <swetamishra.liluah@gmail.com>
2025-04-17 23:48:48 +0530

    typo fix

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -5,7 +5,7 @@ import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
 import usePopularMovies from "../hooks/usePopularMovies";
 import useTopRatedMovies from "../hooks/useTopRatedMovies";
-import useUpcomingMovies from "../hooks/useupcomingMovies";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
 import GptSearch from "./GptSearch";
 import { useSelector } from "react-redux";
 
